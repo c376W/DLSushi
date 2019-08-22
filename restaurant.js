88a864124832894c5af2cbe3bd1ff9aacbf3b6d2
c376W <mailfzc123@gmail.com>
2019-08-22 16:06:00 -0700

    Adding navbar and slider.

---
+++ restaurant.js
@@ -0,0 +1,15 @@
+var navButtons=document.querySelectorAll(".nav-el");
+console.log(navButtons);
+
+// navButtons.addEventListener("click",function(){
+// 	console.log(navButtons.length);
+// 	this.style.color="#f03740";
+// });
+// navButtons.forEach(el=>{
+
+// 	el.addEventListener("click",function(){
+// 		this.style.color="#f03740";
+
+// 	})
+	
+// });
