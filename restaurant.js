a5be3c81d0e138ad5286e059cb26ca01e826285d
c376W <mailfzc123@gmail.com>
2019-09-09 10:19:19 -0700

    Adding navbar news links.

--- restaurant.js
+++ restaurant.js
@@ -3,7 +3,16 @@ console.log(navbar);
 var navButton=document.querySelector(".collapse-hb");
 var collapsed=document.querySelector(".collapsed-choices");
 var flyaway=document.querySelector(".fly-away-wrapper");
+var newsDropdown=document.querySelector(".news-dropdown");
+var newsLink=document.querySelector(".news-link");
+console.log(newsDropdown);
 
+newsLink.addEventListener("mouseover",function(){
+	newsDropdown.style.display="block";
+});
+navbar.addEventListener("mouseleave",function(){
+	newsDropdown.style.display="none";
+})
 
 navButton.addEventListener("click",function(){
 	if(collapsed.classList[1]==="collapse-show"){
